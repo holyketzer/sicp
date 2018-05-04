e8c0dcbc0d8a586b057de0348348d3631b0eea13
Alex Emelyanov <holyketzer@gmail.com>
2018-05-04 15:02:17 +0300

    Solved 2.06-2.08

---
+++ 02/2_07.scm
@@ -0,0 +1,36 @@
+(define (puts x)
+  (display x)
+  (newline))
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (upper-bound interval)
+  (max (car interval) (cdr interval)))
+
+(define (lower-bound interval)
+  (min (car interval) (cdr interval)))
+
+(define (add-interval x y)
+  (make-interval
+    (+ (lower-bound x) (lower-bound y))
+    (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let
+    ((p1 (* (lower-bound x) (lower-bound y)))
+      (p2 (* (lower-bound x) (upper-bound y)))
+      (p3 (* (upper-bound x) (lower-bound y)))
+      (p4 (* (upper-bound x) (upper-bound y))))
+
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x
+    (make-interval
+      (/ 1.0 (upper-bound y))
+      (/ 1.0 (lower-bound y)))))
+
+(puts (add-interval (make-interval 1 5) (make-interval 2 3)))
+(puts (mul-interval (make-interval 1 5) (make-interval 2 3)))
+(puts (div-interval (make-interval 1 5) (make-interval 2 3)))
