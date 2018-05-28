a51d2fb5dde93da04424b38f51e6ba9b217c1a46
Alex Emelyanov <holyketzer@gmail.com>
2018-05-28 09:51:06 +0300

    Solved 2.56-2.57

---
+++ 02/2_57.scm
@@ -0,0 +1,102 @@
+(define (puts x)
+  (display x)
+  (newline))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+    ((variable? exp)
+      (if (same-variable? exp var) 1 0))
+    ((sum? exp)
+      (make-sum
+        (deriv (addend exp) var)
+        (deriv (augend exp) var)))
+    ((product? exp)
+      (make-sum
+        (make-product
+          (multiplier exp)
+          (deriv (multiplicand exp) var))
+        (make-product
+          (deriv (multiplier exp) var)
+          (multiplicand exp))))
+    ((exponentiation? exp)
+      (let (
+        (n (exponent exp))
+        (b (base exp)))
+          (make-product
+            n
+            (make-exponentiation b (make-sum n -1)))))
+    (else
+      (error "неизвестный тип выражения -- DERIV" exp))))
+
+(define (variable? x)
+  (symbol? x))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond
+    ((=number? a1 0) a2)
+    ((=number? a2 0) a1)
+    ((and (number? a1) (number? a2)) (+ a1 a2))
+    (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond
+    ((or (=number? m1 0) (=number? m2 0)) 0)
+    ((=number? m1 1) m2)
+    ((=number? m2 1) m1)
+    ((and (number? m1) (number? m2)) (* m1 m2)) (else (list '* m1 m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s)
+  (cadr s))
+
+(define (augend s)
+  (if (= (length s) 3)
+    (caddr s)
+    (make-sum
+      (addend (cdr s))
+      (augend (cdr s)))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p)
+  (cadr p))
+
+(define (multiplicand p)
+  (if (= (length p) 3)
+    (caddr p)
+    (make-product
+      (multiplier (cdr p))
+      (multiplicand (cdr p)))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (make-exponentiation base n)
+  (cond
+    ((=number? n 0) 1)
+    ((=number? n 1) base)
+    ((and (number? n) (number? base)) (expt base n))
+    (else (list '** base n))))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+
+(puts (deriv '(+ x 3) 'x))
+(puts (deriv '(* x y) 'x))
+(puts (deriv '(* (* x y) (+ x 3)) 'x))
+
+(puts (deriv '(** x 2) 'x))
+(puts (deriv '(* x y (+ x 3)) 'x))
