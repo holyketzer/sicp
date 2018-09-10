9b1d653bf8aff03e9fa1d2055a7b776a8d96e4db
Alex Emelyanov <holyketzer@gmail.com>
2018-09-10 08:27:39 +0300

    Solved 3.35-3.37

---
+++ 03/3_37.scm
@@ -0,0 +1,30 @@
+(define (celsius-fahrenheit-converter x)
+  (c+
+    (c*
+      (c/ (cv 9) (cv 5))
+      x)
+    (cv 32)))
+
+(define C (make-connector))
+
+(define F (celsius-fahrenheit-converter C))
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier z y x)
+    z))
+
+(define (cv value)
+  (let ((z (make-connector)))
+    (constant value z)
+    z))
